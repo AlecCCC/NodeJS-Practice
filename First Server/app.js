c24bf9802aaf41dc146d7c48d427482476b1d88d
Alec <alecjames1996@aol.com>
2026-01-15 21:15:39 -0500

    First JS Server

--- First Server/app.js
+++ First Server/app.js
@@ -12,6 +12,23 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 
+  if (url === '/message' && method === 'POST') {
+      const body = [];
+      req.on('data',(chunk) =>{
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+  });
+}
     res.setHeader("Content-Type", 'text/html')
     res.write('<html>')
     res.write('<head><title>My first html</title><head>')
