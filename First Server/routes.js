df76670d4606884f7a7763393731e6023490d74d
Alec <alecjames1996@aol.com>
2026-01-24 19:13:52 -0500

    Debug Branch

--- First Server/routes.js
+++ First Server/routes.js
@@ -6,11 +6,12 @@ const requestHandler = (req, res) =>{
 
   if (url === '/') {
       res.write('<html>');
-      res.write('<head><title>Enter Message</title><head>');
+      res.write('<head><title>Enter Message</title></head>');
       res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
       res.write('</html>');
       return res.end();
     }
+    
 
     if (url === '/message' && method === 'POST') {
         const body = [];
@@ -22,6 +23,7 @@ const requestHandler = (req, res) =>{
       return req.on('end', () => {
         const parsedBody = Buffer.concat(body).toString();
         const message = parsedBody.split("=")[1];
+        console.log(parsedBody)
         fs.writeFile('message.txt', message, (err) => {
           res.statusCode = 302;
           res.setHeader('Location', '/');
