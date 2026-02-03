d122949a733b162df6de2f5ba549e45a9a109d45
Alec <alecjames1996@aol.com>
2026-02-03 01:02:31 -0500

    Node express Tutorial

--- First Server/app.js
+++ First Server/app.js
@@ -1,23 +1,17 @@
 const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use( (req, res, next) =>{
 
-    console.log("Middleware")
-    next();
-})
-
-
-app.use( (req, res, next) =>{
-
-    console.log("Another Middleware")
-    // ....
-
+app.use('/add-product', (req, res, next) =>{
+    console.log("Another Middleware");
+    res.send('<h1>Add Product Page</h1>');
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) =>{
+    console.log("Another Middleware");
+    res.send('<h1>Hello from express!</h1>');
+});
 
-server.listen(3000);
+app.listen(3000);
