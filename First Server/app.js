362d2c93edb076187ed13fdba7d2c172bc8825f4
Alec <alecjames1996@aol.com>
2026-02-13 14:14:06 -0500

    Done with this for now

--- First Server/app.js
+++ First Server/app.js
@@ -1,17 +1,24 @@
 const http = require('http');
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 
-app.use('/add-product', (req, res, next) =>{
-    console.log("Another Middleware");
-    res.send('<h1>Add Product Page</h1>');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
-app.use('/', (req, res, next) =>{
-    console.log("Another Middleware");
-    res.send('<h1>Hello from express!</h1>');
-});
 
 app.listen(3000);
